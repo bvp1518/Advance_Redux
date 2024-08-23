22d18c17257dfb14130e550096caba9faf0b073e
bvp1518 <bvp18061992@gmail.com>
2024-08-23 15:04:23 +0530

    till-310. Refresher / Practice: Part 2/2

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 import { uiActions } from '../../store/ui-slice';
 
@@ -8,12 +8,13 @@ const CartButton = (props) => {
   const toggleCartHandler = () => {
     // alert('hellooooo')
     dispatch(uiActions.toggle());
-  };
-
+  }
+    const cartQuanity = useSelector(state => state.cart.totalQuantity);
+  
   return (
     <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuanity}</span>
     </button>
   );
 };
